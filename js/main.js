fdda24c9186f7d2068ca424ef5f2b84e8a29fee1
Victor Kim <vskimjr@gmail.com>
2023-11-16 19:54:31 -0500

    edited global variables

--- js/main.js
+++ js/main.js
@@ -10,9 +10,11 @@ const $allStoriesList = $("#all-stories-list");
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
 
-const $submitForm = $("#submit-form");
+const $createForm = $("#create-form");
 // ?TODO: add $myFavorites
 // ?TODO: add $myStories
+
+const $navSubmitStory = $("#nav-submit-story")
 
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
